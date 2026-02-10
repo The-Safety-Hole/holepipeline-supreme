15b8dc988da2f8095cfe26a5dd0fa2e632c5dd28
Daniel <daniel@KaliRat.bahnhof.se>
2026-02-10 09:59:53 +0100

    RED

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-function randomColor() {
-  const random = Math.floor(Math.random() * 16777215).toString(16);
-  document.body.style.backgroundColor = `#${random}`;
-}
-document.body.addEventListener("click", randomColor);
-
 app.get('/status', (req, res) => {
   res.json({
     status: 'ok',
@@ -25,4 +19,4 @@ app.listen(PORT, () => {
 
 module.exports = app;
 
-app.body.style.backgroundColor = "red";
+app.body.style.backgroundColor = red;
