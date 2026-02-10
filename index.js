3a8157eff501e80fa74552f490423d1e0159ec57
Daniel <daniel@KaliRat.bahnhof.se>
2026-02-10 10:11:53 +0100

    added text

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/status', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.send('First Pipeline Challenge - Week 4 GET REKT EVRY OTHER TEAM');
+  res.send('First Pipeline Challenge - Week 4 GET REKT EVRY OTHER TEAM\nNEW LINE I AM SO COOL');
 });
 
 app.listen(PORT, () => {
